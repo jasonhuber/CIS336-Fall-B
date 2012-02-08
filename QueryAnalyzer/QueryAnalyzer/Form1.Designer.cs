baac2ea96ea87e86029aa87cc63748348c75c5c8
unknown <Jason@.(none)>
2012-02-07 20:13:39 -0700

    added the code for the queryanalyzer

---
+++ QueryAnalyzer/QueryAnalyzer/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace QueryAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConnString = new System.Windows.Forms.TextBox();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Grid1 = new System.Windows.Forms.DataGridView();
+            this.lblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtConnString
+            // 
+            this.txtConnString.Location = new System.Drawing.Point(47, 22);
+            this.txtConnString.Name = "txtConnString";
+            this.txtConnString.Size = new System.Drawing.Size(458, 20);
+            this.txtConnString.TabIndex = 0;
+            this.txtConnString.Text = "Provider=SQLNCLI;Server=windows2003base\\sqlexpress;Database=Jason;Uid=sa; Pwd=pas" +
+                "sword;";
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Location = new System.Drawing.Point(49, 61);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(455, 177);
+            this.txtSQL.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(430, 254);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(343, 254);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "connectionstring";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "sql";
+            // 
+            // Grid1
+            // 
+            this.Grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid1.Location = new System.Drawing.Point(47, 283);
+            this.Grid1.Name = "Grid1";
+            this.Grid1.Size = new System.Drawing.Size(455, 165);
+            this.Grid1.TabIndex = 6;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(208, 466);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 497);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.Grid1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.txtConnString);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConnString;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Grid1;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ QueryAnalyzer/QueryAnalyzer/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QueryAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+      {
+          lblError.Text = "";
+            //open connection to database
+
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+            conn.ConnectionString = txtConnString.Text;
+            conn.Open();
+            // MessageBox.Show(conn.State.ToString());
+            //create my command
+            System.Data.OleDb.OleDbCommand comm = new System.Data.OleDb.OleDbCommand();
+            comm.Connection = conn;
+            //send the sql through the command
+            comm.CommandText = txtSQL.Text;
+
+            //receive the result into a data container.
+
+            System.Data.OleDb.OleDbDataReader dr;
+            try
+            {
+
+                if (txtSQL.Text.ToUpper().StartsWith("SELECT"))
+                {
+                    dr = comm.ExecuteReader();
+                    System.Data.DataTable dt = new DataTable();
+                    dt.Load(dr);
+                    Grid1.AutoGenerateColumns = true;
+                    //bind the result to the grid
+
+                    Grid1.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show(comm.ExecuteNonQuery().ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lblError.Text = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            
+            }
+
+            
+       
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtSQL.Text = "";
+        }
+    }
+}
